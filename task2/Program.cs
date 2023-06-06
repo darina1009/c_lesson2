7086e9ce66a27508c1098dae1fb443db1a7ad7f9
darina1009 <darina_1009@mail.ru>
2023-06-06 18:43:15 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿void Task2(){
+    
+
+    Console.WriteLine("Введите  число");
+    string input = Console.ReadLine();
+    int num1 = Convert.ToInt32(input);
+    int num2 = num1 % 10;
+
+    if (input.Length > 2){
+        Console.WriteLine(input[2]);
+
+    } else{
+         Console.WriteLine("третьей цифры нет");
+       // Сonsole.WriteLine();
+    }
+
+   
+
+}
+Task2();
