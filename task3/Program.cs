b54fc009b6cab6e67997478f76487d006f9d39f2
darina1009 <darina_1009@mail.ru>
2023-06-06 18:56:08 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число дня недели");
+string week = Console.ReadLine();
+
+switch (week)
+{
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    Console.WriteLine("нет");
+    break;
+    case "6":
+    case "7":
+    Console.WriteLine("да");
+    break;
+    default:
+    Console.WriteLine("Ввели не правильное число, нужно от 1 до 7");
+    break;
+}
