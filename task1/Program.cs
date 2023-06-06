6a7501be9a1fa37a8e6d778d1a26fa3dc64262fd
darina1009 <darina_1009@mail.ru>
2023-06-06 18:05:44 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿void Task1(){
+    
+
+    Console.WriteLine("Введите трехзначное число");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1 < 100 || num1 >= 1000){
+        Console.WriteLine("Ввели не трехзначное число");
+    }
+    
+    int num2 = num1 / 10 % 10;
+    Console.WriteLine($"Вторая цифра {num2}");
+}
+Task1();
